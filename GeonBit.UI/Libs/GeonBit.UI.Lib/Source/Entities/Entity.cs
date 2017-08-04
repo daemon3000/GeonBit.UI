267e48f83cb1d5ee34ecdefc1da84075ba10e4f3
Cristian Geambasu <geambasu.cristi@gmail.com>
2017-08-04 15:18:22 +0300

    moved source code into separate class library project, old project now contains only example code

--- GeonBit.UI/Libs/GeonBit.UI.Lib/Source/Entities/Entity.cs
+++ GeonBit.UI/Libs/GeonBit.UI.Lib/Source/Entities/Entity.cs
@@ -366,6 +366,9 @@ namespace GeonBit.UI.Entities
                     // if not found climb up to parent
                     type = type.BaseType;
                 }
+
+                // should never get here
+                throw new System.Exception("Internal error in getting default size.");
             }
         }
 
